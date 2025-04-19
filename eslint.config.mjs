c1c0c1989057486342755c4ffaba9ccf2c02ba23
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2025-04-19 10:01:02 +0530

    commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,9 +14,6 @@ const eslintConfig = [
   {
     "rules": {
       "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-empty-object-type": "off",
-      "@typescript-eslint/no-empty-interface": "off",
     }
   },
 ];
