97e33e39064c122e0705abc204ba83bf25f61f47
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2025-05-27 23:17:13 +0530

    commit

--- src/app/api/v1/dataSummary/productSell/route.ts
+++ src/app/api/v1/dataSummary/productSell/route.ts
@@ -6,10 +6,17 @@ import UserModel from "@/Model/User.model";
 export async function GET(request: Request) {
     try {
         const user_id = await verifyUser()
-        const user{ user: IUser } = UserModel.findById({ _id: user_id })
+        // const user{ user: IUser } = UserModel.findById({ _id: user_id })
 
-        const companyId = user?.companyId
+        // const companyId = user?.companyId
+
+
+        return Response.json({ message: "Invoice created successfully" }, { status: 200 });
+
     } catch (error) {
+        console.log(error);
 
+        return Response.json({ message: "Internal server error", error }, { status: 500 });
     }
+
 }
