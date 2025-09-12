73ab12fb83ed794834bc792e6c306cddfdf79784
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2025-09-12 17:44:03 +0530

    update some kots to views

--- src/app/api/v1/Invoice/[InvoiceId]/delete/route.ts
+++ src/app/api/v1/Invoice/[InvoiceId]/delete/route.ts
@@ -1,17 +1,27 @@
 import { verifyUser } from "@/lib/verifyUser";
 import { NextResponse } from "next/server";
 import InvoiceModel from "@/Model/Invoice.model";
-
+import InvoiceKotModel from "@/Model/KOT.model";
+import UserModel from "@/Model/User.model";
+import CompanyModel from "@/Model/Company.model";
 
 export async function DELETE(req: Request, { params }: { params: Promise<{ InvoiceId: string }> }) {
     try {
 
         const { InvoiceId } = await params;
-        console.log(InvoiceId);
-
         const user_id = await verifyUser();
         if (!user_id) {
             return NextResponse.json({ message: "Unauthorized", success: false }, { status: 401 });
+        }
+        const User = await UserModel.findById({ _id: user_id });
+
+        if (!User) {
+            return NextResponse.json({ message: "User not found", success: false }, { status: 404 });
+        }
+        const companyId = User.companyId;
+        const company = await CompanyModel.findById({ _id: companyId });
+        if (!company) {
+            return NextResponse.json({ message: "Company not found", success: false }, { status: 404 });
         }
         const invoice = await InvoiceModel.findOne({ _id: InvoiceId });
         if (!invoice) {
@@ -19,10 +29,14 @@ export async function DELETE(req: Request, { params }: { params: Promise<{ Invoi
         }
 
         await InvoiceModel.deleteOne({ _id: InvoiceId });
+        await InvoiceKotModel.deleteMany({
+            $and: [
+                { invoiceMongoId: InvoiceId },
+                { companyId: companyId },
+            ],
+        })
         return NextResponse.json({ message: "Invoice deleted successfully", success: true }, { status: 200 });
     } catch (error) {
-        console.log(error);
-
         return NextResponse.json({ message: "Internal Server Error", success: false }, { status: 500 });
     }
 }   
