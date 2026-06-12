3ddd707dc10d9ccb0d6f799106293bce014c0294
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2026-06-12 06:30:44 -0400

    add kot tick to complete

--- src/Components/Main/PrintInvoiceFormate.tsx
+++ src/Components/Main/PrintInvoiceFormate.tsx
@@ -21,7 +21,7 @@ export default function PrintInvoiceFormate({ invoice }: { invoice: any }) {
     }
 
     return (
-        <div className={`print-area w-[20rem] mx-auto p-2 text-black uppercase bg-white ${roboto.variable}`}>
+        <div className={`print-area w-[30rem] mx-auto p-2 text-black uppercase bg-white ${roboto.variable}`}>
             <div className="flex justify-end">
                 {invoice?.BillType == "KOT" ? (
                     <div className="flex flex-row">
